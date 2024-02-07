73bbbe8936797f096c2bf747ec4353b6f9785d01
Maksym Voloshyn <mv.webdew@gmail.com>
2024-02-07 08:33:20 +0200

    test: add simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,78 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const res = 4;
+    const inputRaw = {
+      a: 2,
+      b: 2,
+      action: Action.Add,
+    };
+
+    expect(simpleCalculator(inputRaw)).toBe(res);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const res = 50;
+    const inputRaw = {
+      a: 100,
+      b: 50,
+      action: Action.Subtract,
+    };
+
+    expect(simpleCalculator(inputRaw)).toBe(res);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const res = 625;
+    const inputRaw = {
+      a: 25,
+      b: 25,
+      action: Action.Multiply,
+    };
+
+    expect(simpleCalculator(inputRaw)).toBe(res);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const res = 25;
+    const inputRaw = {
+      a: 250,
+      b: 10,
+      action: Action.Divide,
+    };
+
+    expect(simpleCalculator(inputRaw)).toBe(res);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const res = 27;
+    const inputRaw = {
+      a: 3,
+      b: 3,
+      action: Action.Exponentiate,
+    };
+
+    expect(simpleCalculator(inputRaw)).toBe(res);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const inputRaw = {
+      a: 2,
+      b: 2,
+      action: '?',
+    };
+
+    expect(simpleCalculator(inputRaw)).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const inputRaw = {
+      a: '2',
+      b: 2,
+      action: Action.Add,
+    };
+
+    expect(simpleCalculator(inputRaw)).toBeNull();
   });
 });
